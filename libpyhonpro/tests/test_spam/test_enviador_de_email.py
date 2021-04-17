b1a88cbc7cf9a80c97a62166ec0a49adb0ceab0c
andre <gelhen@gmail.com>
2021-04-17 18:57:53 -0300

    Teste exceções e parametrizações de testes
    close #26

--- libpyhonpro/tests/test_spam/test_enviador_de_email.py
+++ libpyhonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,16 +1,36 @@
-from libpyhonpro.spam.enviador_de_email import Enviador
+from libpyhonpro.spam.enviador_de_email import Enviador, EmailInvalido
+import pytest
 
 
 def test_criar_enviador_de_email():
     enviador= Enviador()
     assert enviador is not None
 
-def test_remetente():
+@pytest.mark.parametrize(
+    'remetente',
+    ['gelhen@gmail.com', 'ti@tozzoalimentos.com.br']
+)
+def test_remetente(remetente):
     enviador = Enviador()
     resultado = enviador.enviar(
-        'gelhen@gmail.com',
+        remetente,
         'gelhen@gmail.com',
         'Cursos PythonPro',
         'Teste envio e-mail'
     )
-    assert 'gelhen@gmail.com' in resultado
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'gelhen']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'gelhen@gmail.com',
+            'Cursos PythonPro',
+            'Teste envio e-mail'
+        )
